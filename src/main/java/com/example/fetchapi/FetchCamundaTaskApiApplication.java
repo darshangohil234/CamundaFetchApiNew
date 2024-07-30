00c0fda1f9ca98aebf8726efbe1f15f9cf0c1042
Darshan Gohil (GD) <darshangohil234@gmail.com>
2024-07-30 12:41:13 +0530

    genericReqRespChanges

--- src/main/java/com/example/fetchapi/FetchCamundaTaskApiApplication.java
+++ src/main/java/com/example/fetchapi/FetchCamundaTaskApiApplication.java
@@ -2,10 +2,22 @@ package com.example.fetchapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * FetchCamundaTaskApiApplication is the entry point of the spring boot application.
+ * This class contains the main method which is used to run the application.
+ * <p> @author Darshan Gohil </p>
+ */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.FetchMethod")
 public class FetchCamundaTaskApiApplication {
 
+	/**
+	 * The main that starts the spring boot application.
+	 *
+	 * @param args command-line arguments passed to the application.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FetchCamundaTaskApiApplication.class, args);
 	}
--- src/main/java/com/example/fetchapi/fetchapicontroller/FetchTaskController.java
+++ src/main/java/com/example/fetchapi/fetchapicontroller/FetchTaskController.java
@@ -2,38 +2,54 @@ package com.example.fetchapi.fetchapicontroller;
 
 import java.io.IOException;
 import java.util.List;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.example.pojo.FetchRequestData;
-import com.example.pojo.FetchResponseData;
-import com.example.pojo.FetchTaskRequest;
-import com.example.pojo.FetchTaskResp;
-import com.example.serviceimpl.TaskListServiceImpl;
-
+import com.example.domain.GenericResponseData;
+import com.example.service.TaskListServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller class for handling requests related to tasklist
+ * <p>
+ * This controller provides endpoints to retrive task details and start process instance
+ * </p>
+ * <p> @author Darshan Gohil </p>
+ */
 @RestController
+//@RequestMapping("/api/v1")
 @RequiredArgsConstructor
-@ComponentScan("com.example.serviceimpl")
-@ComponentScan("com.example.pojo")
 public class FetchTaskController {
-	
+
+	/**
+	 * Tasklistserviceimpl provide to access TaskListServiceImpl available data
+	 */
 	private final TaskListServiceImpl taskListServiceImpl;
-	
-	public FetchTaskController(TaskListServiceImpl taskListServiceImpl) {
-		this.taskListServiceImpl = taskListServiceImpl;
-	}
-	
+
+	/**
+	 * Handles Get request for get tasks from the tasklist
+	 *
+	 * //@param requestBody the field for get tasks using some specific fields
+	 * @return a tasks from tasklist as a {@GenericResponseData<List<Object>>}
+	 * @throws IOException if an I/O error occurs.
+	 */
 	@GetMapping("/getAllTasks")
-	public FetchResponseData<List<Object>> getTasks(@RequestBody FetchRequestData<List<Object>> requestBody) throws IOException {
-		FetchResponseData<List<Object>> response = new FetchResponseData<>();
-		List<Object> inputPayload = requestBody.getInputPayload();
-		response = taskListServiceImpl.getAllTask(inputPayload.get(0));
+	public GenericResponseData<List<Object>> getAllTasks(@RequestBody Object requestBody) throws IOException {
+		GenericResponseData<List<Object>> response = taskListServiceImpl.getAllTask(requestBody);
 		return response;
 	}
 
+	/**
+	 * Handles Post request for start process instance
+	 *
+	 * @param requestBody the field for get process instance id
+	 * @return success message after executing successfully {@FetchResponseData<List<Object>>}
+	 * @throws IOException if an I/O error occurs.
+	 */
+/*	@PostMapping("/startProcess")
+	public GenericResponseData<List<Object>> startProcess(Object requestBody) throws  IOException{
+		GenericResponseData<List<Object>> response = taskListServiceImpl.start(requestBody);
+		return response;
+	}*/
 
 }
